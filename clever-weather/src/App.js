f4635bdb28a553fe79cf01be61fd1298ee767694
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-09 11:26:55 +0100

    added the css and the API Integration

--- clever-weather/src/App.js
+++ clever-weather/src/App.js
@@ -6,8 +6,8 @@ import Weather from"./Weather.js";
 function App() {
   return (
     <div className="App">
-   <Weather />
-   <footer>This project was coded by Cristina Mihalachi and it is opepn-sourced on GitHub.</footer>
+   <Weather defaultCity="New York"/>
+   <footer className="text-center">This project was coded by Cristina Mihalachi and it is opepn-sourced on GitHub.</footer>
        </div>
   );
 }
--- clever-weather/src/Weather.js
+++ clever-weather/src/Weather.js
@@ -1,46 +1,80 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from"axios";
+import "./Weather.css";
+import "./App.js";
 
 
-export default function Weather(){
+export default function Weather(props){
+  
+   const[ready, setReady]=useState(false);
+   const[weatherData, setWeatherData]=useState();
 
+   function handleResponse(response){
+    
+      setWeatherData(
+         {
+         temperature:response.data.main.temp,
+          humidity:response.data.main.humidity,
+          city:response.data.name,
+          wind:response.data.wind.speed,
+          description:response.data.weather[0].description,
+          iconUrl:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.creativefabrica.com%2Fwp-content%2Fuploads%2F2021%2F03%2F31%2Fweather-icon-illustration03-Graphics-10205167-1.jpg&tbnid=dLL2bxvENuyu9M&vet=12ahUKEwjIx6me9bSFAxXyygIHHSF2CPUQMygBegQIARBY..i&imgrefurl=https%3A%2F%2Fwww.creativefabrica.com%2Fde%2Fproduct%2Fweather-icon-illustration-03-2%2F&docid=rF6zIn6U5dmFMM&w=3400&h=2200&q=weather%20icon&ved=2ahUKEwjIx6me9bSFAxXyygIHHSF2CPUQMygBegQIARBY",
+          date:"Wednesday 07:00"
 
+         }
+      )
+    
+      setReady(true);
+   }
 
-    return (
-    <div className="Weather"> 
+   if (ready){  return (
+    <div className="Weather "> 
     <form >
         <div className="row">
             <div className="col-9">
               <input type="search"
                      placeholder="Enter a city.."
-                      className="form-control" />
+                      className="form-control"
+                       autoFocus="on"/>
              </div>
              <div className="col-3">
                <input type="submit"
                       value="Search" 
-                    className="btn btn-primary" />
+                    className="btn btn-primary w-100" />
             </div>
          </div>
     </form>
-    <h1>New York</h1>
+    <h1>{weatherData.city}</h1>
        <ul>
-         <li>Wednesday 07:00</li>
-         <li>Mostly Cloudy</li>
+         <li>{weatherData.date} </li>
+         <li className="text-capitalize">{weatherData.description}</li>
       </ul>
 
          <div className="row">
             <div className="col-6">
              <img
-                   src={Weather.icon} 
-                   alt="weather icon"  />
-              6 °C
+                   src={weatherData.iconUrl} 
+                   alt={weatherData.description}  />
+             <span className="temperature">{Math.round(weatherData.temperature)}</span>
+             <span className="unit">°C</span>
              </div>
              <div className="col-6">
                <ul>
-                <li>Precipitation</li> 
-                <li>Humidity</li>
-               <li>Wind</li>
+
+                <li>Humidity:{weatherData.humidity}%</li>
+               <li>Wind:{weatherData.wind}hm/h</li>
                </ul>
             </div>
  </div>
  </div>)
+
+   }
+   else { 
+     let city="New York";
+      const apiKey="97bed167ec49bff56e6c1b63daef9c86";
+const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`
+axios.get(apiUrl).then(handleResponse);
+
+return "Loading..."}
+
 }
