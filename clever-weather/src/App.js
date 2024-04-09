92e5ea7c12b39a555523f887c342aa6e64e8909c
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-09 14:01:45 +0100

    Update App.js

--- clever-weather/src/App.js
+++ clever-weather/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
    <Weather defaultCity="New York"/>
-   <footer className="text-center">This project was coded by Cristina Mihalachi and it is <a href="https://github.com/cristina793/clever-weather" targe="_blank" rel="noreferrer> opepn-sourced on GitHub</a> .</footer>
+   <footer className="text-center">This project was coded by Cristina Mihalachi and it is <a href="https://github.com/cristina793/clever-weather" target="_blank" rel="noreferrer"> open-sourced on GitHub </a> .</footer>
        </div>
   );
 }
