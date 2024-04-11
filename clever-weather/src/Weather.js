216e32e336b9f32dd3d1aad551bcb1daaaa03bca
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-11 09:03:20 +0100

    made a new install

--- clever-weather/src/Weather.js
+++ clever-weather/src/Weather.js
@@ -7,28 +7,23 @@ import WeatherInfo from "./WeatherInfo.js"
 
 export default function Weather(props){
   
-   const[ready, setReady]=useState(false);
+   
    const[weatherData, setWeatherData]=useState({ready: false});
    const[city, setCity]=useState(props.defaultCity);
 
-   function handleResponse(response){
-    
-      setWeatherData(
-         {
-         ready:true,
-         temperature:response.data.main.temp,
-          humidity:response.data.main.humidity,
-          city:response.data.name,
-          wind:response.data.wind.speed,
-          description:response.data.weather[0].description,
-          iconUrl:response.data.weather[0].icon,
-          date:new Date (response.data.dt*1000)
-
-         }
-      )
-    
-      setReady(true);
-   }
+   function handleResponse(response) {
+      setWeatherData({
+        ready: true,
+        coordinates: response.data.coord,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        date: new Date(response.data.dt * 1000),
+        description: response.data.weather[0].description,
+        icon: response.data.weather[0].icon,
+        wind: response.data.wind.speed,
+        city: response.data.name,
+      });
+    }
    function search(){
 
    const apiKey="97bed167ec49bff56e6c1b63daef9c86";
@@ -47,33 +42,34 @@ export default function Weather(props){
       
    }
 
-   if (weatherData.ready){ 
-  return (
-    <div className="Weather"> 
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-            <div className="col-9">
-              <input 
-            type="search"
-            placeholder="Enter a city.."
-            className="form-control"
-            autoFocus="on"
-            onChange={handleCityChange} />
-             </div>
-             <div className="col-3">
-               <input type="submit"
-                      value="Search" 
-                    className="btn btn-primary w-100"
-                    />
+   if (weatherData.ready) {
+      return (
+        <div className="Weather">
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Enter a city.."
+                  className="form-control"
+                  autoFocus="on"
+                  onChange={handleCityChange}
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-100"
+                />
+              </div>
             </div>
-         </div>
-    </form>
-    <WeatherInfo data={weatherData} />
-    </div>
-    );
-   }
-   else { 
-search();
-return "Loading...";}
-
-}
+          </form>
+          <WeatherInfo data={weatherData} />
+        </div>
+      );
+    } else {
+      search();
+      return "Loading...";
+    }
+  }
