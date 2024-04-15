6f773060f8ed682be4fc242fc82596ce2846ac6c
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-15 12:28:51 +0100

    added the loop to forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -10,10 +10,18 @@ export default function WeatherForecastDay(props){
         let temperature=Math.round(props.data.temp.min);
         return `${temperature}°`;
     }
+    function day(){
+        let date = new Date(props.data.dt * 1000);
+        let day = date.getDay();
+
+        let days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+
+        return days[day];
+    }
 
     return(
 <div>
-<div className="WeatherForecastDay">{props.data.dt}</div> 
+<div className="WeatherForecastDay">{day()}</div> 
          <WeatherIcon code={props.data.weather[0].icon} size={36}/> 
             <div className="WeatherForecast-temperatures">
              <span className="WeatherForecast-temperature-max">{maxTemperature()}</span>
